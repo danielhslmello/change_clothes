9f362d1799015b1299a4d2fbaad3aba6feabf0e5
danielhslmello <150366606+danielhslmello@users.noreply.github.com>
2025-03-10 11:40:23 -0300

    novo commit

--- src/components/ChangeClothes.js
+++ src/components/ChangeClothes.js
@@ -34,7 +34,7 @@ const ChangeClothes = () => {
       img.src = URL.createObjectURL(file);
       img.onload = () => {
         if (img.width < 512 || img.height < 512 || img.width > 4096 || img.height > 4096) {
-          alert("Resolução inválida! A largura mínima é  512px e a máxima é 4096px.");
+          alert("Resolução inválida! A largura mínima é  512px e  a  máxima é 4096px.");
           resolve(false);
         } else {
           resolve(true);
